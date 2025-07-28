7e49457e9e629038ff8e58581c7706c09a22a7e6
Mega Nugraha <monmega110@gmail.com>
2025-07-28 10:01:04 +0700

    reworking the sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,19 @@
 export default function sitemap() {
   return [
     {
-      url: "https://iniwebumkm.x1nx3r.uk",
+      url: "https://kelurahankemayoran.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://iniwebumkm.x1nx3r.uk/profil",
+      url: "https://kelurahankemayoran.com/profil",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://iniwebumkm.x1nx3r.uk/umkm",
+      url: "https://kelurahankemayoran.com/umkm",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
